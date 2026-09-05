ae3da99b99d38381dafbf5030df1b889b17208ed
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-09-04 22:58:12 -0700

    completed

--- LogInPage-MVVM/Views/Components/LoginHeaderView.swift
+++ LogInPage-MVVM/Views/Components/LoginHeaderView.swift
@@ -16,12 +16,21 @@ struct LoginHeaderView: View {
                 .ignoresSafeArea(edges: .top)
 
             VStack(spacing: LoginSpacing.small) {
-                logoMark
-                    .padding(.bottom, 2)
+                //logoMark
+                  //  .padding(.bottom, 2)
 
-                Text("Welcome Back")
-                    .font(.system(size: 27, weight: .bold))
-                    .foregroundColor(AppTheme.onAccentPrimary)
+                VStack(spacing: 4) {
+                    Text("Welcome to")
+                        .font(.system(size: 18, weight: .medium)) // smaller size
+                        .foregroundColor(AppTheme.onAccentPrimary)
+
+                    Text("𝓓𝓮𝓿𝓣𝓮𝓬𝓱𝓩𝓪𝓱𝓲𝓭")
+                        .font(.system(size: 27, weight: .bold))   // larger size
+                        .foregroundColor(AppTheme.onAccentPrimary)
+                }
+                .multilineTextAlignment(.center)
+
+
 
                 Text("Sign in to access your wallet and\nmanage your assets securely.")
                     .font(.system(size: 12, weight: .medium))
